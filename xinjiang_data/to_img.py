3db38bfac6af2baa5a0d1a75cd92badd4b3c0e7b
Matthew Gordon <matthew.gordon@tri.global>
2022-12-15 20:14:28 -0800

    process images

--- xinjiang_data/to_img.py
+++ xinjiang_data/to_img.py
@@ -1,8 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+import json
 
 TARGET_SIZE = (240, 240)
+IMAGE_DIR = "/Users/matthewgordon/repo/never_forget/xinjiang_data/xinjiang detainees full_files/"
 def resize(fn):
-	img = Image.open(fn)
+	img = Image.open(IMAGE_DIR + fn)
 	w, h = img.size
 	offset = (w - h)/2
 	if w > h:
@@ -13,4 +15,16 @@ def resize(fn):
 	assert img.size[0] == img.size[1]
 	return img.resize(TARGET_SIZE)
 
-#resize('xinjiang detainees full_files/20180705123752969_653121199905.jpg').show()
+def text_image(d, font_size=20):
+	name = d['name']
+	age = d['age']
+	img = resize(d['img'])
+	I1 = ImageDraw.Draw(img)
+	font = ImageFont.truetype('AmericanTypewriter.ttc',font_size)
+	I1.text((0, 0), f'Name: {name}', fill=(0, 0, 0), font=font)
+	I1.text((0, font_size), f'Age: {age}', fill=(0, 0, 0), font=font)
+	return img
+
+with open('detainees.json', 'r') as f:
+	j = json.load(f)
+	text_image(j[0]).show()
