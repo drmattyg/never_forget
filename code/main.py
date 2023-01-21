7dd33f8f3762e8837a806f2d8c51875a0e40d985
Matthew Gordon <matthew.gordon@tri.global>
2023-01-21 12:18:30 -0800

    done

--- code/main.py
+++ code/main.py
@@ -10,21 +10,28 @@ BL = 18
 bus = 0
 device = 0
 
-IMG_DIR = '/usr/pi/img'
+IMG_DIR = '/home/pi/fmn/img'
 
-FADE_STEPS = 20
-IMAGE_TIME = 20 # sec
-FADE_TIME = 5 # sec
+FADE_STEPS = 100
+IMAGE_TIME = 10 # sec
+FADE_TIME = 10 # sec
 FADE_SLEEP = FADE_TIME/FADE_STEPS
-
 class Fader:
-    def __init__(self, im1, im2, fade_steps):
+    def __init__(self, im1, im2, fade_steps, precompute=False):
         self.im1 = im1
         self.im2 = im2
         self.im_black = Image.new(mode=im1.mode, size=im1.size)
         self.fade_steps = fade_steps
         self.max_step = int(fade_steps / 2)
         self.n = 0
+        self.precompute = precompute
+        if precompute:
+                print('precomputing')
+                self.imgs = self.do_precompute()
+
+    def do_precompute(self):
+       imgs = [x for x in self]
+       return imgs 
 
     def __iter__(self):
         return self
@@ -39,8 +46,8 @@ class Fader:
             return Image.blend(self.im_black, self.im2, alpha=(self.n - self.max_step) / (self.max_step - 1))
 
     @staticmethod
-    def from_filenames(fn1, fn2, fade_steps=FADE_STEPS):
-        return Fader(Image.open(fn1), Image.open(fn2), fade_steps=fade_steps)
+    def from_filenames(fn1, fn2, fade_steps=FADE_STEPS, img_dir=IMG_DIR):
+        return Fader(Image.open(os.path.join(img_dir, fn1)), Image.open(os.path.join(img_dir, fn2)), fade_steps=fade_steps)
 
 if __name__ == '__main__':
 
@@ -58,6 +65,9 @@ if __name__ == '__main__':
             fader = Fader.from_filenames(imgs[n], imgs[n+1], FADE_STEPS)
             disp.ShowImage(fader.im1)
             time.sleep(IMAGE_TIME)
+            if fader.precompute:
+                fader = fader.imgs
+                
             for im in fader:
                 time.sleep(FADE_SLEEP)
                 disp.ShowImage(im)
